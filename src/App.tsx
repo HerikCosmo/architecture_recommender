4fd9b544164a1208f0289befe3e5dbf91e061307
Herik Cosmo <herikcosmo.m@gmail.com>
2026-05-06 18:50:21 -0300

    initial config

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-red-600 shadow-lg p-6 bg-white rounded-2xl">
+        Hello World
+      </h1>
+    </div>
+  )
+}
+
+export default App
